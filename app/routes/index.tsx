594bba2a909da40514728d3df0f3d682f02a9a3a
Christoph Jerolimov <jerolimov+git+2022@gmail.com>
2022-02-06 01:16:22 +0100

    Update index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,17 @@
+import { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+  return { title: "azado" };
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>azado</h1>
+    <div className="p-8 mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+      <h1 className="text-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        {'The '}
+        <span className="text-amber-900">azado</span>
+        {' way to grill.'}
+      </h1>
     </div>
   );
 }

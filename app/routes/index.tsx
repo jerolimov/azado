b55cc8878e883f8818bd05042227433591bf69a0
Christoph Jerolimov <jerolimov+git+2022@gmail.com>
2022-02-06 01:18:12 +0100

    Update index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,10 +7,12 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="p-8 mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
-      <h1 className="text-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+      <h1 className="text-center text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
         {'The '}
-        <span className="text-amber-900">azado</span>
-        {' way to grill.'}
+        <span className="text-amber-300">azado</span>
+        {' way to '}
+        <span className="text-amber-300">grill</span>
+        {'.'}
       </h1>
     </div>
   );
